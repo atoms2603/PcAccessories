834acbfd6c2189ca176383a0bb641583d33194e2
Atoms <huyt4242@gmail.com>
2022-04-08 23:20:43 +0700

    Create DbContext

---
+++ PcAccessoriesAPI/PcAccessories.EFCore/Data/PcAccessoriesContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PcAccessories.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcAccessories.EFCore.Data
+{
+    public class PcAccessoriesContext : DbContext
+    {
+        public PcAccessoriesContext(DbContextOptions<PcAccessoriesContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Slider> Sliders { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
+        public DbSet<ProductLove> ProductLoves { get; set; }
+        public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<InvoiceDetail> InvoiceDetails { get; set; }
+    }
+}
