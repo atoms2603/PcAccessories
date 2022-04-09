9b993e40582fad2295b57a9de3f11a3506e3d7af
Atoms <huyt4242@gmail.com>
2022-04-09 13:06:22 +0700

    Create user service

---
+++ PcAccessoriesAPI/PcAccessories.Services/CMS/UserService/IUserService.cs
@@ -0,0 +1,11 @@
+﻿using PcAccessories.Entities.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PcAccessories.Services.CMS.UserService
+{
+    public interface IUserService
+    {
+        Task<List<User>> GetALL();
+    }
+}
---
+++ PcAccessoriesAPI/PcAccessories.Services/CMS/UserService/UserService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PcAccessories.EFCore.Data;
+using PcAccessories.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcAccessories.Services.CMS.UserService
+{
+    public class UserService : IUserService
+    {
+        private readonly PcAccessoriesDbContext _context;
+        public UserService(PcAccessoriesDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<List<User>> GetALL()
+        {
+           return await _context.Users.ToListAsync();
+        }
+    }
+}
