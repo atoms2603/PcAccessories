99bf5fd2ac02346f5b640f20a5a3df7b48516132
Atoms <huyt4242@gmail.com>
2022-04-09 12:08:23 +0700

    Use MySql

---
+++ PcAccessoriesAPI/PcAccessories.EFCore/Data/PcAccessoriesContextFactory.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PcAccessories.EFCore.Data
+{
+    public class PcAccessoriesContextFactory : IDesignTimeDbContextFactory<PcAccessoriesDbContext>
+    {
+        public PcAccessoriesDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("PcAccessoriesConnection");
+            var severVersion = ServerVersion.AutoDetect(connectionString);
+
+
+            var optionsBuilder = new DbContextOptionsBuilder<PcAccessoriesDbContext>();
+            optionsBuilder.UseMySql(connectionString, severVersion);
+
+            return new PcAccessoriesDbContext(optionsBuilder.Options);
+        }
+    }
+}
