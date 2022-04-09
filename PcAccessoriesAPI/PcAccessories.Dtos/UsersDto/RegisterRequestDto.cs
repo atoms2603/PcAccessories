13d2dc7d244a5eda75ca8f6ab3a9f7b5cd0e81b1
Atoms <huyt4242@gmail.com>
2022-04-09 17:13:49 +0700

    Update Dto user

--- PcAccessoriesAPI/PcAccessories.Dtos/UsersDto/RegisterRequestDto.cs
+++ PcAccessoriesAPI/PcAccessories.Dtos/UsersDto/RegisterRequestDto.cs
@@ -8,6 +8,12 @@ namespace PcAccessories.Dtos.UsersDto
 {
     public class RegisterRequestDto
     {
-
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+        public string DisplayName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
     }
 }
