64b023a69df3ed5825007e18682474829c432222
Atoms <huyt4242@gmail.com>
2022-04-16 15:56:59 +0700

    Handle authorize with header swagger

--- PcAccessoriesAPI/PcAccessories.Web.Host/Startup.cs
+++ PcAccessoriesAPI/PcAccessories.Web.Host/Startup.cs
@@ -49,28 +49,29 @@ namespace PcAccessories.WebAPI
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "PcAccessories API", Description = "PcAccessories", Version = "v1" });
-                c.AddSecurityDefinition("bearerAuth", new OpenApiSecurityScheme()
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
-                    Description = "JWT Authorization header using the Bearer scheme. Example: \"Authorization: Bearer {token}\"",
+                    Description ="JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
                     Name = "Authorization",
                     In = ParameterLocation.Header,
                     Type = SecuritySchemeType.ApiKey,
-                    Scheme = JwtBearerDefaults.AuthenticationScheme
+                    Scheme = "Bearer"
                 });
+
                 c.AddSecurityRequirement(new OpenApiSecurityRequirement()
-                  {
+                {
                     {
-                      new OpenApiSecurityScheme
-                      {
-                        Reference = new OpenApiReference
-                          {
-                            Type = ReferenceType.SecurityScheme,
-                            Id = JwtBearerDefaults.AuthenticationScheme
-                          },
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
                         },
-                        new List<string>()
-                      }
-                    });
+                        new string[] {}
+                    }
+                });
             });
 
 
